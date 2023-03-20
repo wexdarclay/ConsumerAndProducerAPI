ca83ab9bd594189c735f7cc499b609b1ed9bfdd2
wexdarclay <david.gurule@wexinc.com>
2023-03-20 16:24:59 -0600

    secrets

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -21,32 +21,31 @@ var builder = Host.CreateDefaultBuilder()
    .ConfigureServices((hostContext, services) =>
    {
 	   services
-		.AddKafka(WexDivision.Health, typeof(Program).Assembly)
-		//ADDING THE CONSUMER FOR: HEALTH.CDB.CARRIER-NOTIFICATION.DATA 
-		.AddConsumer(builder =>
-		{
-			builder
-			.AddTopic("processing.cdb.carrier-notifications", "Liveness")
-			.AddSubject<CDBCarrierNotifications>("health.cdb.carrier-notification.data");
-		})
-		.AddTransient<IHandler<CDBCarrierNotifications>, CDBMessageHandler>()
-	   //ADDING THE CONSUMER FOR: HEALTH.MBE.CARRIER-NOTIFICATION.LOG
-	   //  .AddConsumer(builder =>
-	   //  {
-	   //   builder
-	   //   .AddTopic("logging.mbe.carrier-notifications", "Liveness")
-	   //   .AddSubject<MBECarrierNotificationsData>("health.mbe.carrier-notifications.log");
-	   //  })
-	   //.AddTransient<IHandler<MBECarrierNotificationsLog>, MBEMessageLogHandler>();
-	   //ADDING THE CONSUMER FOR: HEALTH.MBE.CARRIER-NOTIFICATION.LOG-ERROR
-	   .AddConsumer(builder =>
-	   {
-		   builder
-		   .AddTopic("logging.mbe.carrier-notifications", "Liveness")
-		   .AddSubject<MBECarrierNotificationsLogError>("health.mbe.carrier-notification.log-error");
-	   })
-		.AddTransient<IHandler<MBECarrierNotificationsLogError>, MBEMessageLogErrorHandler>();
-
+			.AddKafka(WexDivision.Health, typeof(Program).Assembly)
+			//ADDING THE CONSUMER FOR: HEALTH.CDB.CARRIER-NOTIFICATION.DATA 
+			.AddConsumer(builder =>
+			{
+				builder
+				.AddTopic("processing.cdb.carrier-notifications", "Liveness")
+				.AddSubject<CDBCarrierNotifications>("health.cdb.carrier-notification.data");
+			})
+			.AddTransient<IHandler<CDBCarrierNotifications>, CDBMessageHandler>()
+			//ADDING THE CONSUMER FOR: HEALTH.MBE.CARRIER - NOTIFICATION.LOG
+			.AddConsumer(builder =>
+			{
+				builder
+				.AddTopic("logging.mbe.carrier-notifications", "Liveness")
+				.AddSubject<MBECarrierNotificationsData>("health.mbe.carrier-notifications.log");
+			})
+			.AddTransient<IHandler<MBECarrierNotificationsLog>, MBEMessageLogHandler>()
+			//ADDING THE CONSUMER FOR: HEALTH.MBE.CARRIER - NOTIFICATION.LOG - ERROR
+			.AddConsumer(builder =>
+			{
+				builder
+				.AddTopic("logging.mbe.carrier-notifications", "Liveness")
+				.AddSubject<MBECarrierNotificationsLogError>("health.mbe.carrier-notification.log-error");
+			})
+			.AddTransient<IHandler<MBECarrierNotificationsLogError>, MBEMessageLogErrorHandler>();
    })
    .ConfigureAppConfiguration((hostingContext, config) =>
    {
